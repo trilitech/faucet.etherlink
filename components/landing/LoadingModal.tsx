70eae2cd31820ad67a5c1064d94aea41cd937692
sashaaldrick <sasha@aldrick.dev>
2023-12-22 16:42:39 +0000

    fixed linting errors in LoadingModal.tsx

--- components/landing/LoadingModal.tsx
+++ components/landing/LoadingModal.tsx
@@ -5,8 +5,8 @@ import etherlinkSpinner from "../../public/images/logo.webp";
 const LoadingModal = ({ loading, selectedToken }) => {
   return (
     <Dialog closable={false} visible={loading} style={{ width: "50vw" }}>
-      <Image width={200} height={200} src={etherlinkSpinner} id="loading" className="text-center mx-auto" />
-      <p className="text-center font-medium mt-4">Drip! Drip! Dripping you some {selectedToken}'s.... </p>
+      <Image width={200} height={200} src={etherlinkSpinner} alt="Loading spinner" id="loading" className="text-center mx-auto" />
+      <p className="text-center font-medium mt-4">Drip! Drip! Dripping you some {selectedToken}&apos;s.... </p>
     </Dialog>
   );
 };
