ced725f5e4533e6e5e61a472f81f246098d5a0f9
Sasha Aldrick <sasha@aldrick.dev>
2024-01-04 15:39:48 +0000

    all good

--- pages/api/faucet/handler.js
+++ pages/api/faucet/handler.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
     if (req.method !== 'POST') {
-        res.status(405).send({ message: 'Only POST requests allowed' })
-        return
+      res.status(405).send({ message: 'Only POST requests allowed' })
+      return
     }
+    res.status(200).json({message: 'All good!'});
 
 }
