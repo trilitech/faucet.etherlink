19a0819ab1734dcbb331ac24454faca66c352600
Sasha Aldrick <sasha@aldrick.dev>
2024-01-04 16:18:47 +0000

    maxFeePerGas

--- pages/api/faucet.js
+++ pages/api/faucet.js
@@ -8,6 +8,8 @@ export default async function handler(req, res) {
             const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
             
             const feeData = await provider.getFeeData();
+
+            const maxFeePerGas = feeData.maxFeePerGas;
             // const transaction = {
             //     to: walletAddress,
             //     value: parseEther("0.1"),
@@ -16,7 +18,7 @@ export default async function handler(req, res) {
 
             // const txResponse = await wallet.sendTransaction(transaction);
             // const receipt = await txResponse.wait();
-            res.status(200).json({walletAddress, feeData});
+            res.status(200).send(maxFeePerGas);
         } else {
             res.setHeader('Allow', 'POST');
             res.status(405).send('Method Not Allowed');
