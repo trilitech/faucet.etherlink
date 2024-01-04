5d31c4e965e3f7e395542d8a4253637e835fe781
Sasha Aldrick <sasha@aldrick.dev>
2024-01-04 16:02:03 +0000

    see walletAddress

--- pages/api/faucet.js
+++ pages/api/faucet.js
@@ -3,7 +3,7 @@ import { JsonRpcProvider, ethers, parseEther } from 'ethers';
 export default async function handler(req, res) {
     try {
         if (req.method === 'POST') {
-            const { walletAddress } = req.body.walletAddress;
+            const { walletAddress } = req.body;
             // const provider = new JsonRpcProvider("https://node.ghostnet.etherlink.com");
             // const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
             
