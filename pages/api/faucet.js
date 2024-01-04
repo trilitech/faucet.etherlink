0ae00dbe8264836aad81cf6a65e2dba3722f3cec
Sasha Aldrick <sasha@aldrick.dev>
2024-01-04 15:58:44 +0000

    fix typo

--- pages/api/faucet.js
+++ pages/api/faucet.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
             // const txResponse = await wallet.sendTransaction(transaction);
             // const receipt = await txResponse.wait();
-            res.status(200).json({ walletAddress });
+            res.status(200).json({ address });
         } else {
             res.setHeader('Allow', 'POST');
             res.status(405).send('Method Not Allowed');
