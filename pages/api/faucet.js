f2edfd373b7796a1125c025d0f2c7211d7882d33
Sasha Aldrick <sasha@aldrick.dev>
2024-01-04 15:45:42 +0000

    add back walletAddress functionality

--- pages/api/faucet.js
+++ pages/api/faucet.js
@@ -1,8 +1,29 @@
-export default function handler(req, res) {
-    if (req.method !== 'POST') {
-      res.status(405).send({ message: 'Only POST requests allowed' })
-      return
+import { JsonRpcProvider, ethers, parseEther } from 'ethers';
+
+export default async function handler(req, res) {
+    try {
+        if (req.method === 'POST') {
+            const { walletAddress } = req.body;
+            // const provider = new JsonRpcProvider("https://node.ghostnet.etherlink.com");
+            // const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
+            
+            // const feeData = await provider.getFeeData();
+            // const transaction = {
+            //     to: walletAddress,
+            //     value: parseEther("0.1"),
+            //     gasPrice: feeData.gasPrice
+            // };
+
+            // const txResponse = await wallet.sendTransaction(transaction);
+            // const receipt = await txResponse.wait();
+            res.status(200).json({ walletAddress });
+        } else {
+            res.setHeader('Allow', 'POST');
+            res.status(405).send('Method Not Allowed');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
     }
-    res.status(200).json({message: 'All good!'});
 
 }
--- pages/api/faucet/handler.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    if (req.method !== 'POST') {
-      res.status(405).send({ message: 'Only POST requests allowed' })
-      return
-    }
-    res.status(200).json({message: 'All good!'});
-
-}
