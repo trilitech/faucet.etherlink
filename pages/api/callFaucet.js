62a5a7434fab041fd07de956e38f97c39da45050
Sasha Aldrick <sasha@aldrick.dev>
2024-01-04 14:54:31 +0000

    respond with request

--- pages/api/callFaucet.js
+++ pages/api/callFaucet.js
@@ -4,7 +4,7 @@ import { JsonRpcProvider, ethers, parseEther, Network, Wallet } from 'ethers';
 export default async function callFaucet(req, res) {
     try {
         // const { walletAddress } = await request.json()
-        const { walletAddress } = await req.json();
+        // const { walletAddress } = await req.json();
         // const provider = new JsonRpcProvider("https://node.ghostnet.etherlink.com");
         // const wallet = new Wallet(process.env.PRIVATE_KEY, provider);
         
@@ -25,7 +25,7 @@ export default async function callFaucet(req, res) {
         //         status: 200,
         //     },
         // );
-        res.status(200).json({ walletAddress });
+        res.status(200).json({ req });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal Server Error" });
